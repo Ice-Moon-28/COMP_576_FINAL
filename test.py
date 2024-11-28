6c0c658639d67aa2e012909d35bed27a11bf0db7
root <root@instance-20241128-011520.asia-east1-c.c.bubbly-cascade-438215-b0.internal>
2024-11-28 01:36:01 +0000

    feat: init environment.xml

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from transformers import AutoModel, AutoTokenizer
 import torch
 
 # # Specify cache location
-# cache_dir = "./weights/Llama-2-7b-hf"
+cache_dir = "./weights/Llama-2-7b-hf"
 
 # # # Download model and tokenizer
-# model_name = "meta-llama/Llama-2-7b-hf"
-# model = AutoModel.from_pretrained(model_name, cache_dir=cache_dir, torch_dtype=torch.float16)
-# tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir, torch_dtype=torch.float16)
+model_name = "meta-llama/Llama-2-7b-hf"
+model = AutoModel.from_pretrained(model_name, cache_dir=cache_dir, torch_dtype=torch.float16)
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir, torch_dtype=torch.float16)
 
 # print(model, tokenizer)
 
@@ -26,10 +26,10 @@ tokenizer = AutoTokenizer.from_pretrained("roberta-large-mnli", cache_dir="./wei
 
 from transformers import BertModel, BertTokenizer
 
-# from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer
 
-# model = SentenceTransformer('nli-roberta-large', cache_folder='./weights/nli-roberta-large')
-# model.save('./weights/nli-roberta-large')
+model = SentenceTransformer('nli-roberta-large', cache_folder='./weights/nli-roberta-large')
+model.save('./weights/nli-roberta-large')
 
 # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', cache_dir='./weights/bert-base-uncased')
 # model = BertModel.from_pretrained('bert-base-uncased', cache_dir='./weights/bert-base-uncased')
